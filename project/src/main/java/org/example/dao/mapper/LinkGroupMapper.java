543d4bff6a1673bff63d4afae4f400e847f16a41
yijun <3220304324@qq.com>
2025-08-15 23:09:50 +0800

    feat(dao): 添加短链接分组实体和持久层接口

---
+++ project/src/main/java/org/example/dao/mapper/LinkGroupMapper.java
@@ -0,0 +1,10 @@
+package org.example.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.dao.entity.GroupDO;
+
+/**
+ * 短链接分组持久层
+ */
+public interface LinkGroupMapper extends BaseMapper<GroupDO> {
+}
