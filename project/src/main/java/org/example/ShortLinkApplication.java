da7cb224282bca6574b03994cea1d2a13cb5951e
yijun <3220304324@qq.com>
2025-08-16 10:47:08 +0800

    feat(*): 集成 Nacos 服务发现与 Feign 客户端

--- project/src/main/java/org/example/ShortLinkApplication.java
+++ project/src/main/java/org/example/ShortLinkApplication.java
@@ -1,12 +1,14 @@
 package org.example;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("org.example.dao.mapper")
 public class ShortLinkApplication {
     public static void main(String[] args) {
