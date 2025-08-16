06837199b217c689aa0e65b5dad305307dc4773f
yijun <3220304324@qq.com>
2025-08-16 09:11:33 +0800

    feat(dao): 为 ShortLinkMapper 接口添加新方法- 新增 incrementStats 方法

--- project/src/main/java/org/example/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/org/example/dao/mapper/ShortLinkMapper.java
@@ -4,11 +4,22 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.example.dao.entity.ShortLinkDO;
 import org.example.dto.req.ShortLinkRecycleBinPageReqDTO;
+import org.apache.ibatis.annotations.Param;
+
 
 /**
  * 短链接持久层
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+    /**
+     * 短链接访问统计自增
+     */
+    void incrementStats(@Param("gid") String gid,
+                        @Param("fullShortUrl") String fullShortUrl,
+                        @Param("totalPv") Integer totalPv,
+                        @Param("totalUv") Integer totalUv,
+                        @Param("totalUip") Integer totalUip);
+
     /**
      * 分页统计回收站短链接
      */
